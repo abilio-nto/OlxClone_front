9576cd40ee34a22eaa9ea667fc121e75b08b1033
abilio-nto <abiliofcb@gmail.com>
2022-06-16 21:33:24 -0300

    Iniciando front com typescript

---
+++ src/redux/reducers/userReducers.ts
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const slice = createSlice({
+    name:"olx",
+    initialState:{
+
+    },
+    reducers:{
+
+    }
+})
+
+export default slice.reducer;
